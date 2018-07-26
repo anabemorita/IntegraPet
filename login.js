af5ddbb867d9c4fee6a179b029b78eba3748b5a1
Ana Beatriz <anabemorita@gmail.com>
2018-07-26 00:12:59 -0300

    Melhorar funcao de login

--- login.js
+++ login.js
@@ -1,16 +1,20 @@
 const botao = document.querySelector("button");
 const email= document.querySelector("#email");
 const senha= document.querySelector("#senha");
+
 let emailUser = "admin";
 let senhaUser = "aviao123";
 
 function logar(){
     if(email.value === emailUser && senha.value === senhaUser)
-        window.location = "usuario.html";
+        setTimeout(abrirJanela, 1500);
         else{
             alert("Usuário e/ou senha incorretos");
         }
 }
 
+function abrirJanela(){
+    window.location = "usuario.html";
+}
 
 botao.onclick = logar;
